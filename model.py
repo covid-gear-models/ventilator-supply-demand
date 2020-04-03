b7266ef9aef8e002ccbbf194de52a2b5d28b0894
James Trimarco <james.trimarco@gmail.com>
2020-04-03 07:24:27 -0400

    switched to Pandas .rolling for computing ICU needs

--- model.py
+++ model.py
@@ -2,13 +2,10 @@ import math
 import pandas as pd
 import numpy as np
 import scipy.integrate
-
-
 from datetime import timedelta
 import shared
-
-import world_data
-import population
+# import world_data
+# import population
 
 def model(Y, x, N, beta0, days0, beta1, gamma, sigma):
     # :param array x: Time step (days)
@@ -28,8 +25,8 @@ def model(Y, x, N, beta0, days0, beta1, gamma, sigma):
     return dS, dE, dI, dR
 
 
-def solve(model, population, E0, beta0, days0, beta1, gamma, sigma, daysTotal):
-    X = np.arange(daysTotal)  # time steps array
+def solve(model, population, E0, beta0, days0, beta1, gamma, sigma, days_total):
+    X = np.arange(days_total)  # time steps list
     N0 = population - E0, E0, 0, 0  # S, E, I, R at initial step
 
     y_data_var = scipy.integrate.odeint(model, N0, X, args=(population, beta0, days0, beta1, gamma, sigma))
@@ -38,16 +35,11 @@ def solve(model, population, E0, beta0, days0, beta1, gamma, sigma, daysTotal):
     return X, S, E, I, R  # note these are all arrays
 
 
-def run_SEIR(population, intensive_units, date_of_first_infection, date_of_lockdown):
-    COUNTRY = 'Germany'  # e.g. 'all' South Korea' 'France'  'Republic of Korea' 'Italy' 'Germany'  'US' 'Spain'
-    PROVINCE = 'all'  # 'all' 'Hubei'  # for provinces other than Hubei the population value needs to be set manually
-    EXCLUDECOUNTRIES = ['China'] if COUNTRY == 'all' else []  # massive measures early on
-
-    # population = population.get_population(COUNTRY, PROVINCE, EXCLUDECOUNTRIES)
+def run_SEIR(population, intensive_units, date_of_first_infection, date_of_lockdown, mean_days_icu):
 
     # --- external parameters ---
-    daysTotal = 365  # total days to model
-    dataOffset = 'auto'  # position of real world data relative to model in whole days.
+    days_total = 365  # total days to model
+    # dataOffset = 'auto'  # position of real world data relative to model in whole days.
     # 'auto' will choose optimal offset based on matching of deaths curves
 
     E0 = 1  # number of exposed people at initial time step
@@ -76,15 +68,15 @@ def run_SEIR(population, intensive_units, date_of_first_infection, date_of_lockd
     # wild guess! italy:16? germany:4 south korea: 4?  a lot of the mild cases will go undetected  assuming 100% correct tests
     percent_cases_detected = (1.0 - percent_asymptomatic) / 20.0
 
-    timeInHospital = 12
-    timeInfected = 1.0 / gamma  # better timeInfectious?
+    days_in_hospital = 12
+    days_infectious = 1.0 / gamma  # better timeInfectious?
 
     # lag, whole days - need sources
-    presymptomaticLag = round(days_presymptomatic)  # effort probably not worth to be more precise than 1 day
-    communicationLag = 2
-    testLag = 3
-    symptomToHospitalLag = 5
-    hospitalToIcuLag = 5
+    presymptomatic_lag = round(days_presymptomatic)  # effort probably not worth to be more precise than 1 day
+    communication_lag = 2
+    test_lag = 3
+    symptom_to_hospital_lag = 5
+    hospital_to_icu_lag = 5
 
     infectionFatalityRateA = 0.01  # Diamond Princess, age corrected
     infectionFatalityRateB = infectionFatalityRateA * 3.0  # higher lethality without ICU - by how much?  even higher without oxygen and meds
@@ -97,50 +89,62 @@ def run_SEIR(population, intensive_units, date_of_first_infection, date_of_lockd
                 r0 - 1)))  # https://hal.archives-ouvertes.fr/hal-00657584/document page 13
     # doublingTime = (math.log(2.0, math.e) / s1)
 
-    X, S, E, I, R = solve(model, population, E0, beta0, days_before_lockdown, beta1, gamma, sigma, daysTotal)
-
-    # derived arrays
-    F = I * percent_cases_detected
-    U = I * icuRate * timeInHospital / timeInfected  # scale for short infectious time vs. real time in hospital
-    P = I / population * 1_000_000  # probability of random person to be infected
-
-    # timeline: exposed, infectious, symptoms, at home, hospital, ICU
-    F = shared.delay(F,
-                     days_presymptomatic + symptomToHospitalLag + testLag + communicationLag)  # found in tests and officially announced; from I
-    U = shared.delay(U, days_presymptomatic + symptomToHospitalLag + hospitalToIcuLag)  # ICU  from I before delay
-    U = shared.delay(U, round(
-        (timeInHospital / timeInfected - 1) * timeInfected))  # ??? delay by scaling? todo: think this through
-
-    # cumulate found --> cases
-    # FC = np.cumsum(F)
-
-    # estimate deaths from recovered
-    D = np.arange(daysTotal)
-    RPrev = 0
-    DPrev = 0
-    for i, x in enumerate(X):
-        IFR = infectionFatalityRateA if U[i] <= intensive_units else infectionFatalityRateB
-        D[i] = DPrev + IFR * (R[i] - RPrev)
-        RPrev = R[i]
-        DPrev = D[i]
-
-    D = shared.delay(D,
-                     - timeInfected + days_presymptomatic + symptomToHospitalLag + timeInHospital + communicationLag)  # deaths  from R
+    X, S, E, I, R = solve(model, population, E0, beta0, days_before_lockdown, beta1, gamma, sigma, days_total)
 
     demand_dict = {'days': X,
                    'susceptible': S,
                    'exposed': E,
                    'infectious': I,
                    'recovered': R,
-                   'deaths': D,
                    }
 
     df = pd.DataFrame(demand_dict)
     df['date'] = df['days'].apply(lambda x: date_of_first_infection + timedelta(days=x))
     df = df.applymap(lambda x: round(x) if isinstance(x, float) else x)
 
+
+    # Feature engineering
+    df['needs_icu'] = df.apply(lambda x: round(x['infectious'] * icuRate), axis=1)
+    df['needs_icu'] = df['needs_icu'].shift(10).rolling(window=mean_days_icu).sum()
+    df.drop(columns=['exposed'], inplace=True)
+
+
+    # # derived arrays
+    # F = I * percent_cases_detected
+    # needs_icu = I * icuRate * days_in_hospital / days_infectious  # scale for short infectious time vs. real time in hospital
+    # # P = I / population * 1_000_000  # probability of random person to be infected
+    #
+    # # timeline: exposed, infectious, symptoms, at home, hospital, ICU
+    # F = shared.delay(F,
+    #                  days_presymptomatic + symptom_to_hospital_lag + test_lag + communication_lag)  # found in tests and officially announced; from I
+    # U = shared.delay(needs_icu, days_presymptomatic + symptom_to_hospital_lag + hospital_to_icu_lag)  # ICU  from I before delay
+    # U = shared.delay(U, round(
+    #     (days_in_hospital / days_infectious - 1) * days_infectious))  # ??? delay by scaling? todo: think this through
+    #
+    # # cumulate found --> cases
+    # # FC = np.cumsum(F)
+    #
+    # # estimate deaths from recovered
+    # D = np.zeros(days_total)
+    # RPrev = 0
+    # DPrev = 0
+    # for i, x in enumerate(X):
+    #     IFR = infectionFatalityRateA if U[i] <= intensive_units else infectionFatalityRateB
+    #     D[i] = DPrev + IFR * (R[i] - RPrev)
+    #     RPrev = R[i]
+    #     DPrev = D[i]
+    #
+    # D = shared.delay(D,
+    #                  - days_infectious + days_presymptomatic
+    #                  + symptom_to_hospital_lag + days_in_hospital
+    #                  + communication_lag)  # deaths  from R
+
+
+
+
+
     line_plot_data = df.melt(id_vars=['date'],
-                             value_vars=['susceptible', 'exposed', 'infectious', 'recovered', 'deaths'],
+                             value_vars=['susceptible', 'infectious', 'recovered', 'needs_icu'],
                              value_name='count',
                              var_name='type')
 
